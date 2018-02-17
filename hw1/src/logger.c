02e203db568ff53a9a1bc9a2c732cb0602674b05
Isaac Ramos <bldkaiser@gmail.com>
2018-02-17 03:11:46 +0000

    replaced tabs for missing file

--- hw1/src/logger.c
+++ hw1/src/logger.c
@@ -4,9 +4,9 @@
 #include "logger.h"
 
 void log_it(const char* tag, const char* msg){
-	time_t now;
-	time(&now);
-	if(verbose){
-		printf("%s [%s]: %s\n", ctime(&now), tag, msg);
-	}
+        time_t now;
+        time(&now);
+        if(verbose){
+                printf("%s [%s]: %s\n", ctime(&now), tag, msg);
+        }
 }
