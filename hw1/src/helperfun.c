9468d347a4533f9adb37676700ff9c62e69769ed
Jay Patel <0807Jpatel@gmail.com>
2018-02-17 20:32:18 -0500

    strcmp issue

--- hw1/src/helperfun.c
+++ hw1/src/helperfun.c
@@ -1,15 +1,29 @@
 #include "helperfun.h"
+#include <errno.h>
 
 void login(char* name, int sockfd){
     int n;
     char buff[30];
-    char* intialMess = "ME2U\r\n\r\n";
-    write(sockfd, intialMess, sizeof(intialMess));
+
+    dprintf(sockfd, "ME2U\r\n\r\n");
+
     if( (n = read(sockfd, buff, sizeof(buff))) < 0 ){
         printf("Error in initial server message");     
     }
-    printf("%s", buff);
-    if( strcmp(buff, "U2EM\r\n\r\n") ){        
-        printf("Error in initial server message");     
+    buff[n] = '\0';
+    printf(" %s %d\n", buff, strcmp(buff, "U2EM\r\n\r\n") );
+    if( strcmp(buff, "U2EM\r\n\r\n") != 0){        
+        printf("error in u2em");
     }
+
+    dprintf(sockfd, "IAM %s\r\n\r\n", name);
+    if( (n = read(sockfd, buff, sizeof(buff))) < 0 ){
+        printf("receinved IAM error");     
+    }
+    if( strcmp(buff, "ETAKEN\r\n\r\n") == 0){        
+        printf("User already in use");
+    }else if( strcmp(buff, "MAI\r\n\r\n" ) == 0) {
+        printf("Error in adding user");
+    }
+
 }
