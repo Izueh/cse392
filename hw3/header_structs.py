0442d18a94e9fab015ae11fddcd0d19f0e691941
0807Jpatel <0807Jpatel@gmail.com>
2018-04-22 13:25:44 -0400

    added getaddr

--- hw3/header_structs.py
+++ hw3/header_structs.py
@@ -2,9 +2,9 @@ from construct.core import Struct, Int16ub
 
 difuse_request = Struct(
         op=Int16ub,
-        size=Int16ub,
+        length=Int16ub,
     )
 
 difuse_response = Struct(
         status=Int16ub,
-        size=Int16ub)
+        length=Int16ub)
